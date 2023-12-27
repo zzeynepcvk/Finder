db62db13df7259c1adaa7f85d4c07f77b65357a2
İrem <95523411+Irem44@users.noreply.github.com>
2023-12-27 14:31:10 +0300

    Delete backend directory

--- backend/KullaniciController.java
+++
@@ -1,4 +0,0 @@
-package com.example.finderproject.projeclass.GenelSınıflar.controller;
-
-public class KullaniciController {
-}
--- backend/KullaniciService.java
+++
@@ -1,8 +0,0 @@
-package com.example.finderproject.projeclass.GenelSınıflar.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class KullaniciService {
-
-}
