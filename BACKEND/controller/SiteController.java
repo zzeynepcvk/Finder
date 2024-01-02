f46411794282f0d65179e8bcc480d1043e306051
İrem <95523411+Irem44@users.noreply.github.com>
2024-01-02 16:45:29 +0300

    Delete BACKEND directory

--- BACKEND/controller/SiteController.java
+++
@@ -1,7 +0,0 @@
-package com.example.finderproject.projeclass.GenelSınıflar.controller;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-@CrossOrigin(origins = "http://frontend-domaini.com")
-public class SiteController {
-}
